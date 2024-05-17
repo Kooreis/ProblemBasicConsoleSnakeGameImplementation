532ca4fde97e1969ad5bd0bf04a223ac6718efb7
Kooreis <kooreis.official@gmail.com>
2024-05-17 09:10:06 -0500

    This first part of the script imports the necessary libraries and initializes the screen with the curses library. It also sets the screen dimensions and creates a new window with these dimensions.

--- solution.py
+++ solution.py
@@ -1,5 +1,3 @@
-Here is a simple implementation of a snake game in the console using Python:
-
 ```python
 import random
 import curses
@@ -10,57 +8,4 @@ sh, sw = s.getmaxyx()
 w = curses.newwin(sh, sw, 0, 0)
 w.keypad(1)
 w.timeout(100)
-
-snk_x = sw//4
-snk_y = sh//2
-snake = [
-    [snk_y, snk_x],
-    [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
-]
-
-food = [sh//2, sw//2]
-w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
-
-key = curses.KEY_RIGHT
-
-while True:
-    next_key = w.getch()
-    key = key if next_key == -1 else next_key
-
-    if snake[0][0] in [0, sh] or \
-        snake[0][1]  in [0, sw] or \
-        snake[0] in snake[1:]:
-        curses.endwin()
-        quit()
-
-    new_head = [snake[0][0], snake[0][1]]
-
-    if key == curses.KEY_DOWN:
-        new_head[0] += 1
-    if key == curses.KEY_UP:
-        new_head[0] -= 1
-    if key == curses.KEY_LEFT:
-        new_head[1] -= 1
-    if key == curses.KEY_RIGHT:
-        new_head[1] += 1
-
-    snake.insert(0, new_head)
-
-    if snake[0] == food:
-        food = None
-        while food is None:
-            nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
-            ]
-            food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_PI)
-    else:
-        tail = snake.pop()
-        w.addch(int(tail[0]), int(tail[1]), ' ')
-
-    w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
 ```
-
-This game uses the curses library to draw on the console. The snake is represented as a list of coordinates, with the head of the snake at the beginning of the list. The game loop checks for user input and moves the snake in the appropriate direction. If the snake runs into the screen border or itself, the game ends. If the snake eats the food, a new piece of food is placed on the screen at a random location, and the snake grows in length.
