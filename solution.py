6d438207b0ca944bbce240968700cae95910eaa8
Kooreis <kooreis.official@gmail.com>
2024-05-18 08:10:06 -0500

    This part of the script sets the initial position of the snake and the food. The snake is represented as a list of coordinates, with the head of the snake at the beginning of the list. The initial direction of the snake is set to the right.

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 ```python
-import random
-import curses
+snk_x = sw//4
+snk_y = sh//2
+snake = [
+    [snk_y, snk_x],
+    [snk_y, snk_x-1],
+    [snk_y, snk_x-2]
+]
 
-s = curses.initscr()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
-w.keypad(1)
-w.timeout(100)
+food = [sh//2, sw//2]
+w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
+
+key = curses.KEY_RIGHT
 ```
