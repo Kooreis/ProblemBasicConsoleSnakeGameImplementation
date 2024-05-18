ff20e22af8dbdfabc31436ea3066f92450c04449
Kooreis <kooreis.official@gmail.com>
2024-05-18 08:10:06 -0500

    This is the Game class. It defines the game board, the snake, and the fruit. It also initializes the game state in the constructor.

--- solution.java
+++ solution.java
@@ -1,10 +1,18 @@
-import java.util.LinkedList;
-import java.util.Random;
-import java.util.Scanner;
+class Game {
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
+    private static final char EMPTY = ' ';
+    private static final char SNAKE = 'S';
+    private static final char FRUIT = 'F';
 
-public class Main {
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
+    private char[][] board;
+    private LinkedList<Point> snake;
+    private Point fruit;
+
+    public Game() {
+        board = new char[HEIGHT][WIDTH];
+        snake = new LinkedList<>();
+        snake.add(new Point(WIDTH / 2, HEIGHT / 2));
+        fruit = new Point(new Random().nextInt(WIDTH), new Random().nextInt(HEIGHT));
     }
 }
